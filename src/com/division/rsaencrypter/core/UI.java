b1ba84e529212ad9819a345adf57824cbc07baf2
mastershake71 <evan.cleary@hotmail.com>
2013-04-08 20:11:10 -0400

    Remove unused function.

--- src/com/division/rsaencrypter/core/UI.java
+++ src/com/division/rsaencrypter/core/UI.java
@@ -179,15 +179,6 @@ public class UI extends javax.swing.JFrame {
         return builder.toString();
     }
 
-    public String bytesToHex(byte[] b) {
-        String result = "";
-        for (int i = 0; i < b.length; i++) {
-            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
-        }
-        return result;
-
-    }
-
     /**
      * @param args the command line arguments
      */
